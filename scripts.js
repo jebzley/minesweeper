31d31468803fa4bd1fd43c4e9f03544657376627
Seb Lee <sebastian.ma.lee@gmail.com>
2021-02-17 22:55:04 +0000

    added checks for neighbour cells, need to implement recursion

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ class Cell {
   constructor(column, row) {
     this.column = column;
     this.row = row;
-    this.id = this.row + "" + this.column;
+    this.id = `cell${this.row}${this.column}`;
     this.isBomb = false;
     this.hasFlag = false;
     this.adjacentBombCount = 0;
@@ -11,10 +11,12 @@ class Cell {
 
 // Populate the game grid with divs because I'm too lazy to make 100 divs
 
-const renderBoard = div => {
-  for (let i = 0; i < 100; i++)
-  div.innerHTML += `<div id='${i}' class='cell'></div>`;
-}
+const renderBoard = (div) => {
+  for (let i = 0; i < 100; i++) {
+    if (i < 10) div.innerHTML += `<div id='cell0${i}' class='cell'></div>`;
+    else div.innerHTML += `<div id='cell${i}' class='cell'></div>`;
+  }
+};
 
 renderBoard(document.getElementById("game-grid"));
 
@@ -43,65 +45,48 @@ placeBombs(gameArr);
 
 console.log(gameArr);
 
+const findAdjacentCells = (cell, arr) => {
+  const adjacentCellArr = [];
+
+  adjacentCellArr[0] = arr.find(
+    (object) => object.id == `cell${cell.row}${cell.column - 1}`
+  );
+  adjacentCellArr[1] = arr.find(
+    (object) => object.id == `cell${cell.row}${cell.column + 1}`
+  );
+  adjacentCellArr[2] = arr.find(
+    (object) => object.id == `cell${cell.row - 1}${cell.column}`
+  );
+  adjacentCellArr[3] = arr.find(
+    (object) => object.id == `cell${cell.row + 1}${cell.column}`
+  );
+  adjacentCellArr[4] = arr.find(
+    (object) => object.id == `cell${cell.row - 1}${cell.column - 1}`
+  );
+  adjacentCellArr[5] = arr.find(
+    (object) => object.id == `cell${cell.row - 1}${cell.column + 1}`
+  );
+  adjacentCellArr[6] = arr.find(
+    (object) => object.id == `cell${cell.row + 1}${cell.column - 1}`
+  );
+  adjacentCellArr[7] = arr.find(
+    (object) => object.id == `cell${cell.row + 1}${cell.column + 1}`
+  );
+  return adjacentCellArr;
+};
+
 // Increment values on each cell depending on how many adjacent bombs it has
 const calculateAdjacentCell = (gameArr) => {
-  gameArr.forEach((cell, i, arr) => {
-    const colLeft = arr.find(
-      (object) => object.id == `${cell.row}${cell.column - 1}`
-    );
-    const colRight = arr.find(
-      (object) => object.id == `${cell.row}${cell.column + 1}`
-    );
-    const rowUp = arr.find(
-      (object) => object.id == `${cell.row - 1}${cell.column}`
-    );
-    const rowDown = arr.find(
-      (object) => object.id == `${cell.row + 1}${cell.column}`
-    );
-    const diagUpLeft = arr.find(
-      (object) => object.id == `${cell.row - 1}${cell.column - 1}`
-    );
-    const diagUpRight = arr.find(
-      (object) => object.id == `${cell.row - 1}${cell.column + 1}`
-    );
-    const diagDownLeft = arr.find(
-      (object) => object.id == `${cell.row + 1}${cell.column - 1}`
-    );
-    const diagDownRight = arr.find(
-      (object) => object.id == `${cell.row + 1}${cell.column + 1}`
-    );
+  gameArr.forEach((cell) => {
+    const adjacentCells = findAdjacentCells(cell, gameArr);
 
     let adjacentBombs = 0;
 
-    if (colLeft != undefined) {
-      if (colLeft.isBomb) adjacentBombs++;
-    }
-    if (colRight != undefined) {
-      if (colRight.isBomb) adjacentBombs++;
-    }
-
-    if (rowUp != undefined) {
-      if (rowUp.isBomb) adjacentBombs++;
-    }
-
-    if (rowDown != undefined) {
-      if (rowDown.isBomb) adjacentBombs++;
-    }
-
-    if (diagUpLeft != undefined) {
-      if (diagUpLeft.isBomb) adjacentBombs++;
-    }
-
-    if (diagUpRight != undefined) {
-      if (diagUpRight.isBomb) adjacentBombs++;
-    }
-
-    if (diagDownLeft != undefined) {
-      if (diagDownLeft.isBomb) adjacentBombs++;
-    }
-    if (diagDownRight != undefined) {
-      if (diagDownRight.isBomb) adjacentBombs++;
-    }
+    adjacentCells.forEach((adjacentCell) => {
+      if (adjacentCell != undefined) {
+        if (adjacentCell.isBomb) adjacentBombs++;
+      }
+    });
 
     cell.adjacentBombCount = adjacentBombs;
   });
@@ -111,7 +96,6 @@ calculateAdjacentCell(gameArr);
 
 // rendering stuff just for debug purposes
 const renderCells = (cellArr) => {
-
   cellArr.forEach((cell, index) => {
     if (gameArr[index].isBomb) cell.innerHTML = "💣";
     else if (gameArr[index].adjacentBombCount == 0) cell.innerHTML = "";
@@ -121,32 +105,48 @@ const renderCells = (cellArr) => {
 
 renderCells(document.querySelectorAll(".cell"));
 
+const findOccupiedCells = (currentCell, gameArr) => {
+  const adjacentCells = findAdjacentCells(currentCell, gameArr);
+
+  adjacentCells.forEach((adjacentCell) => {
+    // do adjacent cells have a value?
+    if (adjacentCell != undefined && adjacentCell.adjacentBombCount > 0) {
+      // if so, reveal them
+      document.getElementById(`${adjacentCell.id}`).classList.add("revealed");
+    } 
+    // if an adjacent cell doesn't have a value and hasn't already been checked, recursively run this script
+    // once no more cells can be checked, break out of the loop somehow
+  });
+};
+
 // Click events on each cell
 const handleCellClick = (cellArr, gameArr) => {
-
-  cellArr.forEach(cell => {
-    cell.addEventListener('click', (event) => {
-      const currentCell = gameArr.find(object => object.id == event.target.id);
+  cellArr.forEach((cell) => {
+    cell.addEventListener("click", (event) => {
+      const currentCell = gameArr.find(
+        (object) => object.id == event.target.id
+      );
       console.log("cell clicked: " + event.target.id);
       console.log(currentCell);
 
       //  If the user clicks a bomb, game over
-      if(currentCell.isBomb) console.log('BANG!')
-
+      if (currentCell.isBomb) console.log("BANG!");
       //  If the user clicks a cell with no value, check adjacent cells to see if they have values (that aren't bombs) and reveal them if they do
-      else if(currentCell.adjacentBombCount == 0) console.log("empty")
-      //    Call this recursively for each cell checked until values are revealed
-      
+      else if (currentCell.adjacentBombCount == 0) {
+        findOccupiedCells(currentCell, gameArr);
+        // do adjacent cells have a value?
+        // if(adjacentCell != undefined && adjacentCell.adjacentBombCount > 0){
+        // if so, reveal them
+        // document.getElementById(`${adjacentCell.id}`).classList.add("revealed");
+        //}
+
+        // if not, run this function on them
+      }
+
       //  If the user clicks a cell with a number, reveal the cell
-      else console.log("Cell revealed. Amount of bombs nearby: " + currentCell.adjacentBombCount);
-      
-
+      else event.target.classList.add("revealed");
     });
-
   });
-}
+};
 
-handleCellClick(document.querySelectorAll(".cell"), gameArr) 
-
-
-
+handleCellClick(document.querySelectorAll(".cell"), gameArr);
